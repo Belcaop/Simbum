5b91740ca5a9d752b3c384915880358dd5c90198
Belcaop <benjaminfrancis246@gmail.com>
2022-10-10 08:01:00 +0100

    AM

--- simbium_app/pages/api/[tokenId].js
+++ simbium_app/pages/api/[tokenId].js
@@ -4,8 +4,8 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   const image_url = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
   res.status(200).json({
-    name: "Simbium Dev #" + tokenId,
-    descrption: "Simbium Dev is a collection of developers in crypto",
+    name: "Crypto Dev  #" + tokenId,
+    descrption: "Crypto Dev  is a collection of developers in crypto",
     image: image_url + tokenId + ".svg",
   })
 }
