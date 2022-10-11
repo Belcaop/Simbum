deba23f66d0dcfcaf2235c65ff66e5e7b24c67d3
Ben <90224685+Belcaop@users.noreply.github.com>
2022-10-11 08:15:01 +0100

    Update index.js

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS ="0xf2149b8B456483AD628A0f43C2d9b5eaBBb1235D";
-const METADATA_URL = "https://simbum.vercel.app//api/";
+const METADATA_URL = "https://simbum.vercel.app/api/";
 module.exports = {WHITELIST_CONTRACT_ADDRESS,METADATA_URL};
 
